7e7792b13e34d9b3274c96e52b6f0b2a3bc95d79
HP <HP@10.101.112.103>
2024-11-07 15:28:55 +0800

    First Commit!

---
+++ src/main/java/com/mycompany/shareen/Shareen.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.shareen;
+
+/**
+ *
+ * @author HP
+ */
+public class Shareen {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
