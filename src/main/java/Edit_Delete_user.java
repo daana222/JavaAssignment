d30be32f7a339eb0f4eeaecad0169e164a0547fe
HP <HP@10.101.112.103>
2024-11-09 20:24:34 +0800

    First Commit!

--- src/main/java/Edit_Delete_user.java
+++ src/main/java/Edit_Delete_user.java
@@ -72,6 +72,11 @@ public class Edit_Delete_user extends javax.swing.JFrame {
         jComboBox1.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "VIEW SALES REPORT", "VIEW INVENTORY REPORT", "VIEW ITEMS", "VIEW SUPPLIERS", "" }));
         jComboBox1.setToolTipText("");
+        jComboBox1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jComboBox1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -184,6 +189,10 @@ public class Edit_Delete_user extends javax.swing.JFrame {
     
     editDeleteUserFrame.setVisible(true);        // TODO add your handling code here:
     }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jComboBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jComboBox1ActionPerformed
 
     /**
      * @param args the command line arguments
